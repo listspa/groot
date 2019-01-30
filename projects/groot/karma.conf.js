a1fea79650ddc8cc48140c2ad36cd2891733b2bc
Andrea Bergia <a.bergia@list-group.com>
2019-01-30 13:57:42 +0100

    Attempt to run gitlab CI

--- projects/groot/karma.conf.js
+++ projects/groot/karma.conf.js
@@ -25,7 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-web-security',
+          '--disable-gpu'
+        ]
+      }
+    },
     singleRun: false
   });
 };
