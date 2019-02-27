703a5a8299203bbc7f04957737722084639b9951
Andrea Bergia <a.bergia@list-group.com>
2019-02-27 12:16:01 +0100

    Added class `thead-primary` for tables

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -11,11 +11,15 @@ export class HomepageComponent {
       version: '0.4.2', released: false, changes: [
         {
           type: ReleaseChangeType.NEW_FEATURE,
-          html: ` Restyled <code>groot-nav-bar</code>.`
+          html: `Restyled <code>groot-nav-bar</code>.`
         },
         {
           type: ReleaseChangeType.NEW_FEATURE,
-          html: ` Animated the <code>groot-nav-bar</code>.`
+          html: `Animated the <code>groot-nav-bar</code>.`
+        },
+        {
+          type: ReleaseChangeType.NEW_FEATURE,
+          html: `Added a new style for the table: <code>thead-primary</code>.`
         }
       ]
     },
