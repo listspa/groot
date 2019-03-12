8c90a9b767904ebfc7f998d2f26ccdb3a2330dca
Andrea Bergia <a.bergia@list-group.com>
2019-03-12 17:40:10 +0100

    Changed success color

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -5,6 +5,14 @@ import {Release, ReleaseChangeType} from './release.model';
 /* tslint:disable:no-trailing-whitespace */
 const RELEASE_NOTES: Release[] = [
   {
+    version: 'next', released: false, changes: [
+      {
+        type: ReleaseChangeType.NEW_FEATURE,
+        html: `Changed success color to make it far more readable.`
+      },
+    ]
+  },
+  {
     version: '0.8.0', released: true, changes: [
       {
         type: ReleaseChangeType.BREAKING_CHANGE,
