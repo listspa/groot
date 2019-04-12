6583a3eac28f61877a4a2fb0ebce24ae64232971
Andrea Bergia <a.bergia@list-group.com>
2019-04-12 16:41:02 +0200

    Restored default background color for collapsible boxes

--- projects/groot/src/lib/groot-base/components/collapsible-box/collapsible-box.component.ts
+++ projects/groot/src/lib/groot-base/components/collapsible-box/collapsible-box.component.ts
@@ -10,6 +10,7 @@ export class CollapsibleBoxComponent {
   @Input() label: string;
   @Input() open = true;
   @Input() headerClasses: string | string[] | null = null;
+  @Input() bodyClasses: string | string[] | null = 'card-body-box-bg';
   @Output() changeDisplay = new EventEmitter<boolean>();
 
   public toggle(): void {
