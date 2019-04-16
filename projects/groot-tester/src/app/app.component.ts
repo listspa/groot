14f8406ba4dfe212da4b2aa4bdcc517ce298a772
Andrea Bergia <a.bergia@list-group.com>
2019-04-16 18:17:18 +0200

    Reordered the documentation's menus to make it easier to find the most common things

--- projects/groot-tester/src/app/app.component.ts
+++ projects/groot-tester/src/app/app.component.ts
@@ -9,25 +9,39 @@ import {TranslationsLanguageService} from '../../../groot/src/lib/groot-base/ser
 })
 export class AppComponent {
   readonly navBarItems: SimpleNavBarItem[] = [
-    {'label': 'Getting started', 'url': '/docs/getting-started', icon: 'fa fa-star-o'},
+    {
+      'label': 'Documentation', 'url': '/docs', icon: 'fa fa-star-o', children: [
+        {label: 'Getting started', url: '/docs/getting-started', icon: 'fa fa-star-o'},
+        {label: 'Calling the server', url: '/docs/calling-the-server', icon: 'fa fa-server'},
+        {label: 'Translations', url: '/docs/translations-guide', icon: 'fa fa-plane'},
+        {label: 'Stylesheets and skins', url: '/docs/groot-style', icon: 'fa fa-paint-brush'},
+        {label: 'Icons', url: '/docs/icons', icon: 'fa fa-picture-o'},
+        {label: 'Internet explorer', url: '/docs/ie-support', icon: 'fa fa-internet-explorer'},
+        {label: 'Getting help', url: '/docs/getting-help', icon: 'fa fa-ambulance'},
+        {label: 'External references', url: '/docs/external-references', icon: 'fa fa-external-link'},
+      ]
+    },
     {
       'label': 'Components', icon: 'fa fa-flash', url: '/demo', children: [
+        {label: 'Forms', url: '/demo/forms', icon: 'fa fa-pencil-square-o'},
         {label: 'Buttons', url: '/demo/buttons', icon: 'fa fa-hand-pointer-o'},
+        {label: 'Tables', url: '/demo/tables', icon: 'fa fa-table'},
+        {label: 'Table with column selector', url: '/demo/tables-autocol', icon: 'fa fa-table'},
+        {isSeparator: true},
+        {label: 'Footer', url: '/demo/footer', icon: 'fa fa-window-maximize'},
+        {label: 'Nav bar', url: '/demo/nav-bar', icon: 'fa fa-bars'},
+        {label: 'Page title', url: '/demo/page-title', icon: 'fa fa-th-list'},
+        {isSeparator: true},
         {label: 'Tabs', url: '/demo/tabs', icon: 'fa fa-th-large'},
         {label: 'Boxes (accordions)', url: '/demo/boxes', icon: 'fa fa-chevron-circle-down'},
         {label: 'Cards', url: '/demo/cards', icon: 'fa fa-pencil-square'},
-        {label: 'Colors and typography', url: '/demo/colors', icon: 'fa fa-paint-brush'},
         {label: 'Loading indicator', url: '/demo/loading', icon: 'fa fa-refresh'},
         {label: 'Toasts', url: '/demo/toasts', icon: 'fa fa-bell'},
         {label: 'Modals', url: '/demo/modals', icon: 'fa fa-window-restore'},
-        {label: 'Footer', url: '/demo/footer', icon: 'fa fa-window-maximize'},
-        {label: 'Tables', url: '/demo/tables', icon: 'fa fa-table'},
-        {label: 'Table with column selector', url: '/demo/tables-autocol', icon: 'fa fa-table'},
-        {label: 'Forms', url: '/demo/forms', icon: 'fa fa-pencil-square-o'},
         {label: 'Tooltips', url: '/demo/tooltips', icon: 'fa fa-square'},
-        {label: 'Nav bar', url: '/demo/nav-bar', icon: 'fa fa-bars'},
-        {label: 'Page title', url: '/demo/page-title', icon: 'fa fa-th-list'},
         {label: 'Misc small components', url: '/demo/misc-small-components', icon: 'fa fa-ellipsis-h'},
+        {isSeparator: true},
+        {label: 'Colors and typography', url: '/demo/colors', icon: 'fa fa-paint-brush'},
         {label: 'Other utilities', url: '/demo/other-utilities'},
         {label: 'Capabilities', url: '/demo/capabilities'},
       ]
@@ -84,28 +98,28 @@ export class AppComponent {
         icon: 'fa fa-flash',
         children: [
           [
+            {label: 'Forms', url: '/demo/forms', icon: 'fa fa-pencil-square-o'},
             {label: 'Buttons', url: '/demo/buttons', icon: 'fa fa-hand-pointer-o'},
-            {label: 'Tabs', url: '/demo/tabs', icon: 'fa fa-th-large'},
-            {label: 'Boxes (accordions)', url: '/demo/boxes', icon: 'fa fa-chevron-circle-down'},
-            {label: 'Cards', url: '/demo/cards', icon: 'fa fa-pencil-square'},
-            {label: 'Colors and typography', url: '/demo/colors', icon: 'fa fa-paint-brush'}
-          ],
-          [
-            {label: 'Loading indicator', url: '/demo/loading', icon: 'fa fa-refresh'},
-            {label: 'Toasts', url: '/demo/toasts', icon: 'fa fa-bell'},
-            {label: 'Modals', url: '/demo/modals', icon: 'fa fa-window-restore'},
-            {label: 'Footer', url: '/demo/footer', icon: 'fa fa-window-maximize'},
-          ],
-          [
             {label: 'Tables', url: '/demo/tables', icon: 'fa fa-table'},
             {label: 'Tables with columns selector', url: '/demo/tables-autocol', icon: 'fa fa-table'},
-            {label: 'Forms', url: '/demo/forms', icon: 'fa fa-pencil-square-o'},
-            {label: 'Tooltips', url: '/demo/tooltips', icon: 'fa fa-square'},
+          ],
+          [
+            {label: 'Footer', url: '/demo/footer', icon: 'fa fa-window-maximize'},
             {label: 'Nav bar', url: '/demo/nav-bar', icon: 'fa fa-bars'},
             {label: 'Page title', url: '/demo/page-title', icon: 'fa fa-th-list'}
           ],
           [
+            {label: 'Tabs', url: '/demo/tabs', icon: 'fa fa-th-large'},
+            {label: 'Boxes (accordions)', url: '/demo/boxes', icon: 'fa fa-chevron-circle-down'},
+            {label: 'Cards', url: '/demo/cards', icon: 'fa fa-pencil-square'},
+            {label: 'Loading indicator', url: '/demo/loading', icon: 'fa fa-refresh'},
+            {label: 'Toasts', url: '/demo/toasts', icon: 'fa fa-bell'},
+            {label: 'Modals', url: '/demo/modals', icon: 'fa fa-window-restore'},
+            {label: 'Tooltips', url: '/demo/tooltips', icon: 'fa fa-square'},
             {label: 'Misc small components', url: '/demo/misc-small-components', icon: 'fa fa-ellipsis-h'},
+          ],
+          [
+            {label: 'Colors and typography', url: '/demo/colors', icon: 'fa fa-paint-brush'},
             {label: 'Other utilities', url: '/demo/other-utilities'},
             {label: 'Capabilities', url: '/demo/capabilities'}
           ]
