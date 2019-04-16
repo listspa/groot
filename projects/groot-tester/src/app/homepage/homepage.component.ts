2359b07d2697ef5683168acd696f59e2950e5d0a
Andrea Bergia <a.bergia@list-group.com>
2019-04-16 18:12:00 +0200

    Highlight current page in the simple menu items

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -74,6 +74,10 @@ to disable the "clear" icon.`
         html: `The <code>groot-navbar</code> supports titles and separators.`
       },
       {
+        type: ReleaseChangeType.NEW_FEATURE,
+        html: `The <code>groot-navbar</code> highlights the currently selected item.`
+      },
+      {
         type: ReleaseChangeType.BUG_FIX,
         html: `Filters ftDate and ftTime now work even with null or empty values.`
       },
