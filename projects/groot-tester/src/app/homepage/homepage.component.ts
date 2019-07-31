ccc455d0b6c3c36aef79f7c4b58b81521aa1ecd5
Andrea Bergia <a.bergia@list-group.com>
2019-07-31 10:17:13 +0200

    Documented explicitly that we support angular 8

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -5,6 +5,14 @@ import {Release, ReleaseChangeType} from './release.model';
 /* tslint:disable:no-trailing-whitespace */
 const RELEASE_NOTES: Release[] = [
   {
+    version: '0.14.0', released: false, changes: [
+      {
+        type: ReleaseChangeType.NEW_FEATURE,
+        html: 'Supported explicitly angular 8'
+      }
+    ]
+  },
+  {
     version: '0.13.9', released: true, changes: [
       {
         type: ReleaseChangeType.BUG_FIX,
