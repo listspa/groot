3304f96835cc1e509aec828a7cd61fbe81a5ae4b
Andrea Bergia <a.bergia@list-group.com>
2019-07-31 10:36:53 +0200

    Attempts (failed) to publish to npm

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,4 +6,4 @@ if (tgz.length !== 1) {
   throw new Error("Expected to find exactly one tgz file in dist");
 }
 
-spawn('npm.cmd', ['publish', tgz[0]], {stdio: 'inherit'});
+spawn('npm.cmd', ['publish', tgz[0], '--verbose'], {stdio: 'inherit'});
