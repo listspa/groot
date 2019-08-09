b96f4046dd77a6d2a351650553d68e12bb1a6b72
Andrea Bergia <a.bergia@list-group.com>
2019-08-09 17:50:04 +0200

    Added some common translations

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -20,6 +20,10 @@ main app module`
         html: 'New interface <code>SortPagination</code>'
       },
       {
+        type: ReleaseChangeType.NEW_FEATURE,
+        html: 'Added some common translations: <code>common.list, common.view</code>'
+      },
+      {
         type: ReleaseChangeType.BUG_FIX,
         html: `It is now possible to use Groot in a lazy-loaded child module`
       },
