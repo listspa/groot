1c0758a92d8a43d20384b0b2c133d7312b67715b
Andrea Bergia <a.bergia@list-group.com>
2019-08-19 09:03:46 +0200

    feat: improved logging of the copy-demo-app script

--- scripts/release-demo-prj.js
+++ scripts/release-demo-prj.js
@@ -1,10 +1,16 @@
 const client = require('scp2');
 
+console.log('Copying demo app files...');
 client.scp('./dist/groot-tester/', {
   host: '192.168.232.41',
   username: 'root',
   password: 'mariano',
   path: '/work/apache/groot-doc'
 }, err => {
-  console.log(err);
+  if (err) {
+    console.error('Cannot copy files!');
+    console.error(err);
+  } else {
+    console.log('Copy complete!');
+  }
 });
