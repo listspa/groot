0a70c104add91bacbb114e4a861cbe93c02b45b6
Andrea Bergia <a.bergia@list-group.com>
2020-06-08 15:50:14 +0200

    fix: fixed error for radio buttons in a view form with disabled radio

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -5,6 +5,14 @@ import {Release, ReleaseChangeType} from './release.model';
 /* tslint:disable:no-trailing-whitespace */
 const RELEASE_NOTES: Release[] = [
   {
+    version: '0.21.3', released: true, changes: [
+      {
+        type: ReleaseChangeType.BUG_FIX,
+        html: `Error in view form with a radio button disabled`
+      },
+    ],
+  },
+  {
     version: '0.21.2', released: true, changes: [
       {
         type: ReleaseChangeType.NEW_FEATURE,
