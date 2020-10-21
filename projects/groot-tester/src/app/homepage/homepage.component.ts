ab56334beeb1d6819b715c083f5c2517e8433479
Andrea Bergia <a.bergia@list-group.com>
2020-10-21 16:46:26 +0200

    docs: prepared for Angular 10 and release 1.0

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -7,9 +7,17 @@ const RELEASE_NOTES: Release[] = [
   {
     version: '1.0.0', released: true, changes: [{
       type: ReleaseChangeType.BREAKING_CHANGE,
-      html: `Angular 10 is now required. TODO
-      <br>
-      Nota cambiare import scss <code>@import "~@listgroup/groot/src/style/groot";</code>`
+      html: `Angular 10 is now required. This requires in turn updates to the various dependencies:
+<ul>
+    <li>"@ng-select/ng-select": "^5.0.8"</li>
+    <li>"@ngx-translate/core": "^13.0.0"</li>
+    <li>"@ngx-translate/http-loader": "^6.0.0"</li>
+    <li>"ngx-bootstrap": "^6.1.0"</li>
+</ul>`
+    }, {
+      type: ReleaseChangeType.BREAKING_CHANGE,
+  html: `In your CSS files you will need to change Groot import to
+<code>@import "~@listgroup/groot/src/style/groot";</code>`
     }
     ]
   },
