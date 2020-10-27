b93e9e73ecac57c564c0217b15a599251c25aca1
Andrea Bergia <a.bergia@list-group.com>
2020-10-27 16:51:19 +0100

    fix: with Angular 10, we _need_ to use `ng build --prod`

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -5,6 +5,12 @@ import {Release, ReleaseChangeType} from './release.model';
 /* tslint:disable:no-trailing-whitespace */
 const RELEASE_NOTES: Release[] = [
   {
+    version: '1.0.1', released: true, changes: [{
+      type: ReleaseChangeType.BUG_FIX,
+      html: `Fixed a setting with the build for Angular 10 and Ivy. Otherwise, we have problems with Garfield.`
+    }]
+  },
+  {
     version: '1.0.0', released: true, changes: [{
       type: ReleaseChangeType.BREAKING_CHANGE,
       html: `Angular 10 is now required. This requires in turn updates to the various dependencies:
