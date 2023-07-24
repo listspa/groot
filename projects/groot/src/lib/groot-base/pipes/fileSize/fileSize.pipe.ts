afc18ae4c77b7d0e7a91706d2cbcd99ba05eb70f
Samuele Dallape <s.dallape@list-group.com>
2023-07-24 15:02:08 +0200

    feat: upgrade filesize version, avoiding warning during compilation

--- projects/groot/src/lib/groot-base/pipes/fileSize/fileSize.pipe.ts
+++ projects/groot/src/lib/groot-base/pipes/fileSize/fileSize.pipe.ts
@@ -1,11 +1,11 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import * as filesize from 'filesize';
+import { Pipe, PipeTransform } from '@angular/core';
+import { filesize } from 'filesize';
 
 @Pipe({
   name: 'fileSize'
 })
 export class FileSizePipe implements PipeTransform {
-  transform(value: any, round = 1): any {
-    return filesize(value, {round});
+  transform(value: any, round = 1, standard = 'jedec', base = 2): any {
+    return filesize(value, { round, base, standard });
   }
 }
