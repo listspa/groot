22ab2d3c409cd83fd8a7f4eb812749508744e76a
Nicolò Malfatto <n.malfatto@list-group.com>
2023-08-10 16:04:53 +0200

    fix: used proper input type

--- projects/groot/src/lib/groot-base/pipes/fileSize/fileSize.pipe.ts
+++ projects/groot/src/lib/groot-base/pipes/fileSize/fileSize.pipe.ts
@@ -5,7 +5,7 @@ import { filesize } from 'filesize';
   name: 'fileSize'
 })
 export class FileSizePipe implements PipeTransform {
-  transform(value: any, round = 1, standard = 'jedec', base = 2): any {
+  transform(value: any, round = 1, standard: 'iec' | 'jedec' = 'jedec', base = 2): any {
     return filesize(value, { round, base, standard });
   }
 }
