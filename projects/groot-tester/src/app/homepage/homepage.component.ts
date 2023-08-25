1040d5d444c71145d0a4a7cb69991357a6b47077
Alessandro Versace <a.versace@list-group.com>
2023-08-25 15:45:42 +0200

    doc: 4.0.0 release_notes with GRooT migration guide
    jira: ISPGROOT-179

--- projects/groot-tester/src/app/homepage/homepage.component.ts
+++ projects/groot-tester/src/app/homepage/homepage.component.ts
@@ -5,6 +5,47 @@ import {Release, ReleaseChangeType} from './release.model';
 /* tslint:disable:no-trailing-whitespace */
 const RELEASE_NOTES: Release[] = [
   {
+    version: '4.0.0', released: true, changes: [{
+      type: ReleaseChangeType.BREAKING_CHANGE,
+      html: `Updated to Angular 15.2.9. This requires to update your Angular project from v11 to v15 by following the update guide <a href="https://update.angular.io/?v=11.0-15.0">https://update.angular.io/?v=11.0-15.0</a>.`
+    }, {
+      type: ReleaseChangeType.BREAKING_CHANGE,
+      html: `Updated to Bootstrap 5.3. This requires to turn updates to the third parties dependencies:
+        <ul>
+          <li>bootstrap@5</li>
+          <li>ngx-bootstrap@10</li>
+          <li>@ngx-translate/core@14</li>
+          <li>@ngx-translate/http-loader@7</li>
+          <li>@ng-select/ng-select@10</li>
+          <li>ngx-currency@3</li>
+          <li>filesize@10</li>
+          <li>core-js@3.31.1</li>
+        </ul>
+        <p></p>
+        <p>@use and @import scss no longer maps correctly <code>~</code> to <code>node_modules/</code> folder. Solve this by adding the following options in <code>angular.json</code> file under <code>projects.$PROJECT_NAME.architect.build.options</code>:</p>
+        <pre><code>"stylePreprocessorOptions": {
+    "includePaths": [
+      "node_modules/"
+    ]
+},</code></pre>
+
+        <p>and remove <code>~</code> in your scss where necessary.</p>
+
+        <p>Then follow the migration guide from Bootstrap4 to Bootstrap5 available here <a href="https://getbootstrap.com/docs/5.0/migration/">https://getbootstrap.com/docs/5.0/migration/</a>.</p>
+        <p>If your application builds without errors, but you experience the following error (or a similar one) in the browser console:</p>
+
+        <pre><code>Error: NG0203: inject() must be called from an injection context such as a constructor, a factory function, a field initializer, or a function used with \`EnvironmentInjector#runInContext\`</code></pre>
+
+        <p>add in the <code>angular.json</code> file under <code>projects.$PROJECT_NAME.architect.build.options</code> the following property:</p>
+
+        <pre><code>"preserveSymlinks": true,</code></pre>`
+    }, {
+      type: ReleaseChangeType.NEW_FEATURE,
+      html: `Provided default and customizable styles to new bs5 <code>.btn-close</code> by following it with <code>.btn-close-*</code> classes with default bootstrap colors (primary, success, ...), or <code>.btn-close-darwin</code>, or by creating your custom style with the <code>create-btn-close($color, $value, $close-width)</code> mixin.`
+    },
+    ]
+  },
+  {
     version: '3.4.3', released: true, changes: [
       {
         type: ReleaseChangeType.BUG_FIX,
